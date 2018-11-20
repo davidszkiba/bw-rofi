f63c388abf7abddfe6de77c85a5427424362ad69
David Szkiba <david.szkiba@gmx.at>
2018-11-21 00:11:40 +0100

    Use -r option of jq to get rid of sed command

--- bw-rofi.sh
+++ bw-rofi.sh
@@ -2,6 +2,6 @@
 
 # bw list items | jq '.[].name + "\n>> Copy username\n>> Copy password\n>> Open URL\n>> Copy URL"' |sed "s/\"//g" | sed 's/\\n/\n/g' | rofi -dmenu -show
 
-bw list items | jq '.[].name' | sed "s/\"//g"
+bw list items | jq -r '.[].name'
 
 ## TODO: If we are passed a name, check which key was pressed and perform appropriate action.
